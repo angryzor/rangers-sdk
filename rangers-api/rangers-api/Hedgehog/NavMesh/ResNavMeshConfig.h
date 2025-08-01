35691fd89e15d76287ca50ed7a7b0c1f9e9cb276
Ashrindy <honzula1000@gmail.com>
2025-08-01 17:02:02 +0200

    navmesh resources

--- rangers-api/rangers-api/Hedgehog/NavMesh/ResNavMeshConfig.h
+++ rangers-api/rangers-api/Hedgehog/NavMesh/ResNavMeshConfig.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <ucsl/resources/navmeshconfig/v1.h>
 
 namespace hh::navmesh{
     class ResNavMeshConfig : public fnd::ManagedResource {
     public:
-        
+        ucsl::resources::navmeshconfig::v1::NavMeshConfigData* data;
+        int unk0;
+
+        virtual void Load(void* data, size_t size) override;
 
         MANAGED_RESOURCE_CLASS_DECLARATION(ResNavMeshConfig)
     };
--- rangers-api/rangers-api/Hedgehog/NavMesh/ResNavMeshTile.h
+++ rangers-api/rangers-api/Hedgehog/NavMesh/ResNavMeshTile.h
@@ -3,7 +3,11 @@
 namespace hh::navmesh{
     class ResNavMeshTile : public fnd::ManagedResource {
     public:
+        void* headerData;
+        void* subData;
+        int unk0;
         
+        virtual void Load(void* data, size_t size) override;
 
         MANAGED_RESOURCE_CLASS_DECLARATION(ResNavMeshTile)
     };
